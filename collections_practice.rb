04fd7a6992bb6bc3b576a539e1003093eb61dce0
Natalie Taktachev <ntaktach@gmail.com>
2019-07-21 19:35:32 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,12 +3,12 @@ def sort_array_asc(array)
     a <=> b
 end
 end
-#
-# def array_sort_desc(array)
-#   array.sort! { |x, y| y <=> x }
-# end
-# end
-#
-# def array_sort_char_count(array)
-#   array.sort! { |a, b| a.length <=> b.length }
-# end
+
+def sort_array_desc(array)
+  array.sort! { |x, y| y <=> x }
+end
+end
+
+def array_sort_char_count(array)
+  array.sort! { |a, b| a.length <=> b.length }
+end
